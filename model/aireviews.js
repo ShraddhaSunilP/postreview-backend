252cd510c17306fa857d8c14f39e6162e68ca626
ShraddhaSunilP <shraddhapatil2630@gmail.com>
2024-10-28 14:23:35 +0530

    updated

--- model/aireviews.js
+++ model/aireviews.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const aireviewsSchema = new mongoose.Schema({
     merchantInfoId: { type: mongoose.Schema.Types.ObjectId, ref: 'merchantsinfos', required: true, index: true },  
     generateAIReview: { type: String, required: true, trim: true },  
-    ratings: { type: Number, min: 1, max: 5, required: true }, 
+    ratings: { type: Number, min: 0, max: 5, required: true }, 
     question1: { type: String, required: true, trim: true },
     q1SelectedKeywords: { type: [String], required: true },
     question2: { type: String, required: true, trim: true },  
